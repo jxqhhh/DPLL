a0125be12270c364f92ac92fd35ebda2b4018517
jxqhhh <1327166906@qq.com>
2020-03-08 01:16:30 +0800

    backup for CDCL

--- CDCL.cpp
+++ CDCL.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "CDCL.h"
+#include <unordered_map>
+#include <queue>
 
 CDCL::CDCL::~CDCL() {
     if(graph!=nullptr){
@@ -13,12 +15,12 @@ CDCL::CDCL::~CDCL() {
 
 CDCL::CDCL::CDCL(const formula& _phi): phi(_phi){
     //TODO
-    graph = new node[phi.num_variable];
+    graph = new node[phi.num_variable+1];
 }
 
 model CDCL::CDCL::get_model() {
     model m;
-    for(int i=0;i<phi.num_variable;i++){
+    for(int i=1;i<=phi.num_variable;i++){
         if(graph[i].value = _true){
             m[i] = true;
         }else{
@@ -31,8 +33,8 @@ model CDCL::CDCL::get_model() {
 bool CDCL::CDCL::check_sat() {
     while(1){
         while(exists_unit()){};
-        I.decide();
-        if(confilict()){
+        decide();
+        if(conflict()){
             if(! has_decision()){
                 return false;
             }
@@ -43,11 +45,208 @@ bool CDCL::CDCL::check_sat() {
 }
 
 bool CDCL::CDCL::exists_unit() {
+    // outer loop:
     for(auto &clause: phi.clauses){
-        
-        for(auto &literal: clause){
+        bool found = false;
+        int num_undefined = 0;
+        int antecedent;
+        int undefinedliteral;
 
+        // inner loop:
+        int counter = 0;
+        for (auto &_literal: clause) {
+            auto &n = this->graph[VAR(_literal)];
+            counter++;
+            if (!n.assigned) {
+                num_undefined++;
+                if (num_undefined == 1) {
+                    undefinedliteral = _literal;
+                    antecedent = counter;
+                } else {
+                    goto NextOuterLoop; // too many undefined varibales in the clause, indicating the clause is not a unit
+                }
+            } else {
+                bool isPositiveliteral = _literal > 0;
+                bool isPositiveInterpretation = (n.value == _true);
+                if ((isPositiveliteral) && (!isPositiveInterpretation)) {
+                    goto NextInnerLoop;
+                } else if ((!isPositiveliteral) && (isPositiveInterpretation)) {
+                    goto NextInnerLoop;
+                } else {
+                    goto NextOuterLoop;; // the clause is true under current interpretation, indicating the clause is not a unit
+                }
+            }
+
+            NextInnerLoop:
+                ;
         }
+
+        if (num_undefined == 1) {
+            // The clause is a unit!
+            auto &n = this->graph[VAR(undefinedliteral)];
+            n.value = (undefinedliteral > 0) ? _true:_false;
+            n.assigned = true;
+            n.antecedent = antecedent;
+            int max_decision_level = 0;
+            for(auto& _literal:phi.clauses[antecedent-1]){
+                if(_literal!=undefinedliteral){
+                    auto &partner = graph[VAR(_literal)];
+                    partner.descendant_node_index.push_back(VAR(undefinedliteral));
+                    n.antecedent_node_index.push_back(VAR(_literal));
+                    int level = this->graph[VAR(_literal)].decision_level;
+                    if(level>max_decision_level){
+                        max_decision_level = level;
+                    }
+                }
+            }
+            n.decision_level = max_decision_level;
+            return true;
+        }
+
+        NextOuterLoop:
+            ;
+
     }
     return false;
+}
+
+void CDCL::CDCL::decide(){
+    for(int i=1;i<=phi.num_variable;i++){
+        auto &n = graph[i];
+        if(n.assigned){
+            continue;
+        }
+        n.assigned=true;
+        n.antecedent = 0;
+        n.decision_level = current_decision_level++;
+        n.value = _true;
+        return;
+    }
+}
+
+bool CDCL::CDCL::conflict() {
+    int counter = 0;
+    for(auto &_clause: phi.clauses){
+        counter++;
+        for (auto &_literal: _clause) {
+            bool isPositiveLiteral = _literal > 0;
+            auto &n = graph[VAR(_literal)];
+
+            bool isPositiveInterpretation = (n.value==_true);
+            if (!n.assigned) {
+                goto NextOuterLoop; // undefined variable found, indicating no conflict
+            }
+            if (isPositiveLiteral & isPositiveInterpretation) {
+                goto NextOuterLoop;
+            }
+            if (!(isPositiveLiteral || isPositiveInterpretation)) {
+                goto NextOuterLoop;
+            }
+        }
+
+        //A conflict detected!
+        graph[0].assigned = true;
+        graph[0].value = _true;
+        graph[0].antecedent = counter;
+        int max_decision_level = 0;
+        for(auto &_literal: _clause){
+            auto &n = graph[VAR(_literal)];
+            n.descendant_node_index.push_back(0);
+            graph[0].antecedent_node_index.push_back(VAR(_literal));
+            if(n.decision_level>max_decision_level){
+                max_decision_level=n.decision_level;
+            }
+        }
+        graph[0].decision_level = max_decision_level;
+        return true;
+
+        NextOuterLoop:
+            ;
+
+    }
+    return false;
+}
+
+bool CDCL::CDCL::has_decision() {
+    bool decision_found = false;
+    for(int i=1;i<=phi.num_variable;i++){
+        auto &n = graph[i];
+        if(n.antecedent==0 && n.assigned){
+            decision_found = true;
+            break;
+        }
+    }
+    if(decision_found){
+
+        // generate the conflict clause:
+        bool changed;
+        std::unordered_map<int, bool> learnt_clause;
+
+        learnt_clause[0]=true;
+        int decision_level = graph[0].decision_level;
+        do{
+            changed = false;
+            for(auto it=learnt_clause.begin(),it2=learnt_clause.end();it!=it2;it++){
+                auto &n=graph[it->first];
+                if(n.decision_level==decision_level && n.antecedent!=0){
+                    for(auto lit=n.antecedent_node_index.begin(),lit2=n.antecedent_node_index.end();lit<lit2;lit++){
+                        if(learnt_clause.find(*lit)==learnt_clause.end()){
+                            learnt_clause[*lit] = (graph[*lit].value == _true);
+                            changed = true;
+                        }else if(learnt_clause[*lit] != (graph[*lit].value==true)){
+                            learnt_clause.erase(*lit);
+                            changed = true;
+                        }
+                    }
+                    break;
+                }
+            }
+        }while(changed);
+        clause generated_clause;
+        for(auto it=learnt_clause.begin(),it2=learnt_clause.end();it!=it2;it++){
+            if(it->second){
+                generated_clause.push_back(it->first);
+            }else{
+                generated_clause.push_back(-it->first);
+            }
+        }
+
+        // apply the backjump rule:
+        phi.clauses.push_back(generated_clause);
+        std::queue<int> to_be_erased_node_index;
+        int size = 0;
+        for(auto it=learnt_clause.begin(),it2=learnt_clause.end();it!=it2;it++){
+            to_be_erased_node_index.push(it->first);
+            if(graph[it->first].decision_level==decision_level){
+                size++;
+            }
+        }
+        do{
+            int n = to_be_erased_node_index.front();
+            to_be_erased_node_index.pop();
+            auto &_node=graph[n];
+
+            for(auto &index: _node.descendant_node_index){
+                to_be_erased_node_index.push(index);
+            }
+            if(size == 1){
+                _node.assigned = true;
+                _node.decision_level = decision_level;
+                _node.antecedent_node_index.clear();
+                _node.descendant_node_index.clear();
+                for(auto it=learnt_clause.begin(),it2=learnt_clause.end();it!=it2;it++){
+                    if(graph[it->first].decision_level==decision_level){
+                        if(VAR(learnt_clause[it->first])!=n){
+                            _node.antecedent_node_index.push_back(VAR(learnt_clause[it->first]);
+                        }
+                    }
+                }
+            }
+            if(size >= 1 && _node.decision_level == decision_level ){
+                size--;
+            }
+        }while(!to_be_erased_node_index.empty());
+    }else{
+        return true;
+    }
 }
--- CDCL.h
+++ CDCL.h
@@ -6,28 +6,37 @@
 #define DPLL_CDCL_H
 
 #include "common.h"
+#include <vector>
 
-namespace CDCL{
+namespace CDCL {
 
-    enum assignment{ // possible value assignment for propositional variables
+    enum assignment { // possible value assignment for propositional variables
         _true, _false, _undefined
     };
 
-    struct node{
+    struct node {
         bool assigned = false; // indicating whether the node has been assigned; this attribute must be set to false if you undo the assignment
-        enum assignment value=_undefined;
+        enum assignment value = _undefined;
         int decision_level;
         int antecedent = 0; // range: 0 - num_clauses (0 corresponds to decision variables or unassigned variables)
-        int descendant = 0; // range: 0 - num_clause (0 corresponds to the case that no descendant exists in the graph)
-        node():assigned(false), value(_undefined), antecedent(0), descendant(0){}
+        std::vector<int> antecedent_node_index; // range of elements of antecedent_node_index: 1 - num_clause
+        std::vector<int> descendant_node_index; // range of elements of descendant_node_index: 0 - num_clause (0 corresponds to the special conflict node)
+        node() : assigned(false), value(_undefined), antecedent(0) {
+            descendant_node_index.clear();
+            antecedent_node_index.clear();
+        }
     };
 
     class CDCL {
-        CDCL(const CDCL&)=delete;
-        CDCL(CDCL&&)=delete;
-        CDCL()= delete;
+        CDCL(const CDCL &) = delete;
+
+        CDCL(CDCL &&) = delete;
+
+        CDCL() = delete;
+
         ~CDCL();
-        CDCL(const formula& _phi);
+
+        CDCL(const formula &_phi);
 
         /**
          * Check if the formula is satisfiable.
@@ -46,18 +55,35 @@ namespace CDCL{
          * @note Please DON'T CHANGE this signature because the grading script will directly call this function!
          */
         model get_model();
+
     private:
-        node* graph=nullptr;
+        node *graph = nullptr; // graph[0] corresponds to the special conflict node
         formula phi;
+        int current_decision_level = 0;
 
         /**
          * Detect the case that the unit propogation rule can be applied; apply the rule if it is the case
          * @return true iff at least one unit detected
          */
         bool exists_unit();
+
+        /**
+         * If any decision variable exists within current interpretation, apply the backjump rule.
+         * @return true if has any decision variable else false
+         */
+        bool has_decision();
+
+        /**
+         * Apply the decision rule to assign a variable.
+         */
+        void decide();
+
+        /**
+         * @return true if a conflict is detected else false
+         **/
+        bool conflict();
     };
 }
-
 
 
 #endif //DPLL_CDCL_H
